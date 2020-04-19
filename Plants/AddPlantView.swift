4a2f998a2ca2d9876346f88564fd7d97dd6e07d5
Alex <alexanderwdaniel@gmail.com>
2020-04-19 11:20:26 -0700

    Add ability to save a plant with a name and water frequency.

---
+++ Plants/AddPlantView.swift
@@ -0,0 +1,56 @@
+//
+//  AddPlantView.swift
+//  Plants
+//
+//  Created by Alex Daniel on 4/19/20.
+//  Copyright © 2020 adaniel. All rights reserved.
+//
+
+import SwiftUI
+import Combine
+
+struct AddPlantView: View {
+    @Environment(\.managedObjectContext) var moc
+    @Environment(\.presentationMode) var presentationMode
+    
+    @State private var name = ""
+    @State private var waterFrequency = ""
+    @State private var notes = ""
+    
+    var body: some View {
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Plant Name", text: $name)
+                    TextField("Water every", text: $waterFrequency)
+                        .keyboardType(.numberPad)
+                        .onReceive(Just(waterFrequency)) { newValue in
+                            let filtered = newValue.filter { "0123456789".contains($0) }
+                            if filtered != newValue {
+                                self.waterFrequency = filtered
+                            }
+                    }
+                }
+
+                Section {
+                    TextField("About me", text: $notes)
+                }
+
+        
+                Button("Save") {
+                    // add the plant
+                    let plant = Plant(context: self.moc)
+                    plant.name = self.name
+                    if let frequency = Int16(self.waterFrequency) {
+                        plant.waterFrequency = frequency
+                    }
+                    plant.notes = self.notes
+
+                    try? self.moc.save()
+                    self.presentationMode.wrappedValue.dismiss()
+                }
+            }
+            .navigationBarTitle("Add Plant")
+        }
+    }
+}
--- Plants/ContentView.swift
+++ Plants/ContentView.swift
@@ -9,8 +9,24 @@
 import SwiftUI
 
 struct ContentView: View {
+    @Environment(\.managedObjectContext) var moc
+    @FetchRequest(entity: Plant.entity(), sortDescriptors: []) var plants: FetchedResults<Plant>
+
+    @State private var showingAddScreen = false
+    
     var body: some View {
-        Text("Hello, World!")
+        NavigationView {
+            Text("Count: \(plants.count)")
+                .navigationBarTitle("My Plants")
+                .navigationBarItems(trailing: Button(action: {
+                    self.showingAddScreen.toggle()
+                }) {
+                    Image(systemName: "plus")
+                })
+                .sheet(isPresented: $showingAddScreen) {
+                    AddPlantView().environment(\.managedObjectContext, self.moc)
+                }
+        }
     }
 }
 
