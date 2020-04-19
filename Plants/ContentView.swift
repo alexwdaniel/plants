1a9c0df2d65f7239ece0e7bca2527597ed4263a0
Alex Daniel <alexanderwdaniel@gmail.com>
2020-04-19 10:09:03 -0700

    Initial commit

---
+++ Plants/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Plants
+//
+//  Created by Alexander Daniel on 4/19/20.
+//  Copyright © 2020 adaniel. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
