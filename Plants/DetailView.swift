87869dcc70658b4472d6c1147bf50543da1d9ff7
Alex Daniel <alexanderwdaniel@gmail.com>
2020-05-17 16:18:58 -0700

    Move plant details into bottom sheet.

--- Plants/DetailView.swift
+++ Plants/DetailView.swift
@@ -45,6 +45,7 @@ struct DetailView: View {
     @Environment(\.presentationMode) var presentationMode
     @State var plant: Plant
     @State private var showingDeleteAlert = false
+    @State private var bottomSheetShown = false
     
     static let dateFormat: DateFormatter = {
         let formatter = DateFormatter()
@@ -52,73 +53,81 @@ struct DetailView: View {
         return formatter
     }()
     
-    var body: some View {
-        VStack(alignment: .leading, spacing: 20) {
-            if plant.uiImage != nil {
-                Image(uiImage: plant.uiImage!).resizable().aspectRatio(contentMode: .fit).frame(maxWidth: .infinity)
+    var body: some View {                        
+        GeometryReader { geometry in
+            if self.plant.uiImage != nil {
+                Image(uiImage: self.plant.uiImage!).resizable().aspectRatio(nil, contentMode: .fill).position(CGPoint(x: geometry.size.width / 2, y: geometry.size.height / 2))
+            } else {
+                Color("Pink")
             }
             
-            Text(plant.name ?? "Unknown").font(.system(size: 24, weight: .bold, design: .rounded)).foregroundColor(Color("Gray")).frame(maxWidth: .infinity, alignment: .center)
-            
-            Button(action: {
-                let watering = Watering(context: self.moc)
-                watering.happenedAt = Date()
-                self.plant.addToWaterings(watering)
-                
-                // reset reminder
-                Notifications.cancelReminder(plant: self.plant)
-                Notifications.water(plant: self.plant)
-                
-                try? self.moc.save()
-            }) {
-                Text("WATER").textStyle(ButtonTextStyle()).frame(maxWidth: .infinity, alignment: .center)
-            }
-            
-            Divider()
-            
-            Group {
-                HStack {
-                    Text("FREQUENCY").textStyle(LabelStyle())
-                    if plant.waterFrequency > 0 {
-                        Text("\(plant.waterFrequency) days").textStyle(ValueStyle())
-                    } else {
-                        Text("-").textStyle(ValueStyle())
-                    }
-                }
-                
-                if plant.latestWatering != nil {
-                    HStack {
-                        Text("WATERED").textStyle(LabelStyle())
-                        Text("\(plant.latestWatering!.happenedAt!, formatter: Self.dateFormat)").textStyle(ValueStyle())
-                    }
-                }
-                
-                HStack {
-                    Text("REMINDER").textStyle(LabelStyle())
+            BottomSheetView(
+                isOpen: self.$bottomSheetShown,
+                maxHeight: geometry.size.height * 0.7
+            ) {
+                VStack(alignment: .leading, spacing: 20) {
+                    Text(self.plant.name ?? "Unknown").font(.system(size: 24, weight: .bold, design: .rounded)).foregroundColor(Color("Gray")).frame(maxWidth: .infinity, alignment: .center)
+                    
                     Button(action: {
+                        let watering = Watering(context: self.moc)
+                        watering.happenedAt = Date()
+                        self.plant.addToWaterings(watering)
+                        
+                        // reset reminder
                         Notifications.cancelReminder(plant: self.plant)
+                        Notifications.water(plant: self.plant)
+                        
+                        try? self.moc.save()
                     }) {
-                        Text("STOP").textStyle(ButtonTextStyle())
+                        Text("WATER").textStyle(ButtonTextStyle()).frame(maxWidth: .infinity, alignment: .center)
                     }
+                    
+                    Divider()
+                    
+                    Group {
+                        HStack {
+                            Text("FREQUENCY").textStyle(LabelStyle())
+                            if self.plant.waterFrequency > 0 {
+                                Text("\(self.plant.waterFrequency) days").textStyle(ValueStyle())
+                            } else {
+                                Text("-").textStyle(ValueStyle())
+                            }
+                        }
+                        
+                        if self.plant.latestWatering != nil {
+                            HStack {
+                                Text("WATERED").textStyle(LabelStyle())
+                                Text("\(self.plant.latestWatering!.happenedAt!, formatter: Self.dateFormat)").textStyle(ValueStyle())
+                            }
+                        }
+                        
+                        HStack {
+                            Text("REMINDER").textStyle(LabelStyle())
+                            Button(action: {
+                                Notifications.cancelReminder(plant: self.plant)
+                            }) {
+                                Text("STOP").textStyle(ButtonTextStyle())
+                            }
+                        }
+                    }.padding([.leading, .trailing], 20)
+                    
+                    Spacer()
+                    
+                    Group {
+                        Button(action: {
+                            self.showingDeleteAlert = true
+                        }) {
+                            Text("DELETE").textStyle(ButtonTextStyle())
+                        }
+                    }.padding([.leading], 20).padding([.bottom], 40)
+                    
                 }
-            }.padding([.leading, .trailing], 20)
-            
-            Spacer()
-            
-            Group {
-                Button(action: {
-                    self.showingDeleteAlert = true
-                }) {
-                    Text("DELETE").textStyle(ButtonTextStyle())
-                }
-            }.padding([.leading, .trailing], 20)
-            
-            Spacer()
-            
-        }.alert(isPresented: $showingDeleteAlert) {
-            Alert(title: Text("Delete Plant"), message: Text("Are you sure?"), primaryButton: .destructive(Text("Delete")) {
-                self.deletePlant()
-                }, secondaryButton: .cancel())
+            }
+        }.edgesIgnoringSafeArea(.all)
+            .alert(isPresented: $showingDeleteAlert) {
+                Alert(title: Text("Delete Plant"), message: Text("Are you sure?"), primaryButton: .destructive(Text("Delete")) {
+                    self.deletePlant()
+                    }, secondaryButton: .cancel())
         }
     }
     
